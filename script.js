fc7794569c9fd0bee6971ffec03d55e6468c4849
GuugaaBR <gugafmagalhaes15@gmail.com>
2024-06-11 22:13:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -51,11 +51,6 @@ const timeGenerator = () => {
     minutes += 1;
     seconds = 0;
   }
-  //format time before displaying
-  let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
-  let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
-  timeValue.innerHTML = `<span>Time: </span>${minutesValue}:${secondsValue}`;
-};
 
 //For calculating moves
 const movesCounter = () => {
